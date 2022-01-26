697f46a464398655ea9a228eeb4c48f891437d76
Camerooooon <cjpuffnstuff@gmail.com>
2022-01-25 17:02:30 -0800

    Create first client

---
+++ src/client.rs
@@ -0,0 +1,44 @@
+use x11rb::connection::Connection;
+use x11rb::rust_connection::RustConnection;
+use x11rb::protocol::xproto::*;
+use x11rb::COPY_DEPTH_FROM_PARENT;
+
+pub trait Reduc {
+    fn connect_client(&mut self); 
+}
+
+#[derive(Debug, Default)]
+pub struct Client {
+    pub screen_num: usize,
+    pub window_id: u32,
+}
+
+impl Reduc for Client {
+
+    fn connect_client(&mut self) {
+        
+        let (con, num) = x11rb::connect(None).unwrap();
+        self.screen_num = num;
+        self.window_id = con.generate_id().unwrap();
+
+        let screen = &con.setup().roots[self.screen_num];
+        con.create_window(
+            COPY_DEPTH_FROM_PARENT,
+            self.window_id,
+            screen.root,
+            0,
+            0,
+            600,
+            600,
+            0,
+            WindowClass::INPUT_OUTPUT,
+            0,
+            &CreateWindowAux::new().background_pixel(screen.white_pixel),
+        );
+        con.map_window(self.window_id);
+        con.flush();
+        loop {
+            println!("Event: {:?}", con.wait_for_event().unwrap());
+        }
+    }
+}
